6f69f1ac253bb1cc57b9ab026073f5ab69bea2e1
Robert Jin <jzyrobert@googlemail.com>
2023-10-05 02:14:02 +0100

    Play muted

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <div id="outer">
     <div id="inner">
     <!-- svelte-ignore a11y-media-has-caption -->
-    <video autoplay={true} controls={false} loop={true} src="star_brown.mp4"></video>
+    <video autoplay={true} muted={true} controls={false} loop={true} src="star_brown.mp4"></video>
 </div>
 <div id="inner">
     <!-- svelte-ignore a11y-media-has-caption -->
-    <video autoplay={true} controls={false} loop={true} src="prompt.mp4"></video>
+    <video autoplay={true} muted={true} controls={false} loop={true} src="prompt.mp4"></video>
 </div>
 </div>
 
