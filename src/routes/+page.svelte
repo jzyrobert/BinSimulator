effcb62b6bf7cbaf92403bf764bb3925373919e2
Robert Jin <jzyrobert@googlemail.com>
2023-10-05 22:03:00 +0100

    Swap to new video:

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,20 @@
 <div id="outer">
     <div id="inner">
     <!-- svelte-ignore a11y-media-has-caption -->
-    <video autoplay={true} muted={true} controls={false} loop={true} src="star_brown.mp4"></video>
-</div>
-<div id="inner">
-    <!-- svelte-ignore a11y-media-has-caption -->
-    <video autoplay={true} muted={true} controls={false} loop={true} src="prompt.mp4"></video>
+    <video autoplay={true} muted={true} controls={false} loop={true} src="Background_1.mp4"></video>
 </div>
 </div>
 
 <style>
     #outer {
+        height: 100%;
         display: flex;
         justify-content: center;
         flex-direction: column;
     }
     #inner {
         display: flex;
+        align-content: center;
         justify-content: center;
     }
 </style>
