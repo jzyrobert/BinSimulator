39c43a9c776e21c7f5d73a5d055db2140d4e2e88
Robert Jin <jzyrobert@googlemail.com>
2023-10-26 20:51:52 +0100

    Preload and customise volume

---
+++ src/lib/files.ts
@@ -0,0 +1,38 @@
+export const IMAGE_FILES = [
+    "clara",
+    "danheng",
+    "kafka",
+    "march_angry",
+    "march_angry2",
+    "march_photo",
+    "pompom_warning",
+    "pompom",
+    "praise",
+    "welt"
+]
+
+export const AUDIO_FILES = [
+    "clara_interesting",
+    "dan_look",
+    "dan_time",
+    "danheng_path",
+    "kafka_hiding2",
+    "march_better",
+    "march_business",
+    "march_stop_talking",
+    "march_urge",
+    "march_why",
+    "pompom_excited",
+    "pompom_more",
+    "welt_secrets"
+]
+
+export const SOUND_FILES = [
+    "achieve",
+    "click_fast",
+    "Embers",
+]
+
+export const VIDEO_FILES = [
+    "Background_2"
+]
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { TRASHCAN_DIALOGUES, VOICE_LINE_DIALOGUES, ENDING_DIALOGUES } from '$lib/dialogue';
+	import { AUDIO_FILES, IMAGE_FILES, SOUND_FILES, VIDEO_FILES } from '$lib/files';
 	import type { AudioLine } from '$lib/dialogue';
 	import { fade } from 'svelte/transition';
 	import { base } from '$app/paths';
@@ -7,7 +8,10 @@
 
 	function playClick() {
 		if (backgroundAudio != null && !backgroundPlaying) {
-			backgroundAudio.volume = 0.15;
+			backgroundAudio.volume = backgroundVolume;
+			if (validBackgroundVolumeArg) {
+				backgroundAudio.volume = parseFloat(urlParams.get('bgVolume')!!);
+			}
 			backgroundAudio.play();
 			backgroundPlaying = true;
 		}
@@ -45,7 +49,7 @@
 		}
 		lastTextChoice = index;
 		if (validTextIndexArg) {
-			index = Number(urlParams.get("textIndex"))
+			index = Number(urlParams.get('textIndex'));
 		}
 		const entry = TRASHCAN_DIALOGUES[index];
 		if (typeof entry === 'string') {
@@ -96,9 +100,9 @@
 			return;
 		}
 		// Always play the multiline ending dialogue
-		let chance = voiceChance
+		let chance = voiceChance;
 		if (validChanceArg) {
-			chance = Number(urlParams.get("voiceChance"))
+			chance = Number(urlParams.get('voiceChance'));
 		}
 		const play = isMultiLine ? true : Math.random() <= chance;
 		if (play) {
@@ -121,7 +125,7 @@
 				}
 				lastVoicelineChoice = index;
 				if (validVoiceIndexArg) {
-					index = Number(urlParams.get("voiceIndex"))
+					index = Number(urlParams.get('voiceIndex'));
 				}
 				dialogueObj = VOICE_LINE_DIALOGUES[index];
 			}
@@ -133,14 +137,54 @@
 			voiceDialogue = dialogueObj.line;
 			showDialogue = true;
 
+			voiceNode.volume = voiceVolume;
+			if (validVoiceVolumeArg) {
+				voiceNode.volume = parseFloat(urlParams.get('voiceVolume')!!);
+			}
 			voiceNode.play();
 		}
 	}
 
+	function prefetchFiles() {
+		for (const image of IMAGE_FILES) {
+			const newImage = new Image();
+			newImage.src = base + '/images/' + image + '.webp';
+			imageFiles.push(newImage);
+		}
+		for (const audio of AUDIO_FILES) {
+			const newAudio = new Audio();
+			newAudio.src = base + '/voices/' + audio + '.mp3';
+			newAudio.addEventListener('canplaythrough', loadAudio, false);
+			audioFiles.push(newAudio);
+		}
+		for (const audio of SOUND_FILES) {
+			const newAudio = new Audio();
+			newAudio.src = base + '/' + audio + '.mp3';
+			newAudio.addEventListener('canplaythrough', loadAudio, false);
+			audioFiles.push(newAudio);
+		}
+	}
+
+	function loadAudio() {
+		audioLoaded++;
+		if (audioLoaded === AUDIO_FILES.length + SOUND_FILES.length) {
+			loadingFiles = false;
+		}
+	}
+
+	function isNumeric(n) {
+		return !isNaN(parseFloat(n)) && isFinite(n);
+	}
+
 	let urlParams: URLSearchParams;
 	onMount(() => {
-		urlParams = new URLSearchParams(window.location.search)
-	})
+		urlParams = new URLSearchParams(window.location.search);
+		prefetchFiles();
+	});
+
+	let imageFiles: HTMLImageElement[] = [];
+	let audioFiles: HTMLAudioElement[] = [];
+	let audioLoaded = 0;
 
 	let showVideo = true;
 	let backgroundPlaying = false;
@@ -149,6 +193,7 @@
 	let ready = false;
 	let introRunning = false;
 	let showAchievement = false;
+	let loadingFiles = true;
 
 	let voiceDialogue = '';
 	let dialogueIcon = '';
@@ -159,6 +204,8 @@
 	let lastTextChoice = -1;
 	let lastEndingChoice = -1;
 	let lastVoicelineChoice = -1;
+	let voiceVolume = 0.8; // 80%
+	let backgroundVolume = 0.1; // 10%
 	let textDuration = 0;
 	let voiceChance = 0.4; // 40%
 	let textSpeed = 70; // Duration = Length of text divided by this number, limited to textDurationLimit (seconds)
@@ -179,14 +226,42 @@
 	];
 	$: isMultiLine = totalVoiceDialogue.length > 1;
 	$: isLastEntry = currentTextIndex == totalVoiceDialogue.length - 1;
-	$: validChanceArg = urlParams != null && urlParams.has("voiceChance") && Number(urlParams.get("voiceChance")) >= 0 && Number(urlParams.get("voiceChance")) <= 1
-	$: validTextIndexArg = urlParams != null && urlParams.has("textIndex") && Number.isInteger(Number(urlParams.get("textIndex"))) && Number(urlParams.get("textIndex")) >= 0 && Number(urlParams.get("textIndex")) < TRASHCAN_DIALOGUES.length
-	$: validVoiceIndexArg = urlParams != null && urlParams.has("voiceIndex") && Number.isInteger(Number(urlParams.get("voiceIndex"))) && Number(urlParams.get("voiceIndex")) >= 0 && Number(urlParams.get("voiceIndex")) < VOICE_LINE_DIALOGUES.length
+	$: validChanceArg =
+		urlParams != null &&
+		urlParams.has('voiceChance') &&
+		Number(urlParams.get('voiceChance')) >= 0 &&
+		Number(urlParams.get('voiceChance')) <= 1;
+	$: validTextIndexArg =
+		urlParams != null &&
+		urlParams.has('textIndex') &&
+		Number.isInteger(Number(urlParams.get('textIndex'))) &&
+		Number(urlParams.get('textIndex')) >= 0 &&
+		Number(urlParams.get('textIndex')) < TRASHCAN_DIALOGUES.length;
+	$: validVoiceIndexArg =
+		urlParams != null &&
+		urlParams.has('voiceIndex') &&
+		Number.isInteger(Number(urlParams.get('voiceIndex'))) &&
+		Number(urlParams.get('voiceIndex')) >= 0 &&
+		Number(urlParams.get('voiceIndex')) < VOICE_LINE_DIALOGUES.length;
+	$: validVoiceVolumeArg =
+		urlParams != null &&
+		urlParams.has('voiceVolume') &&
+		isNumeric(urlParams.get('voiceVolume')) &&
+		parseFloat(urlParams.get('voiceVolume')!!) >= 0 &&
+		parseFloat(urlParams.get('voiceVolume')!!) <= 1;
+	$: validBackgroundVolumeArg =
+		urlParams != null &&
+		urlParams.has('bgVolume') &&
+		isNumeric(urlParams.get('bgVolume')) &&
+		parseFloat(urlParams.get('bgVolume')!!) >= 0 &&
+		parseFloat(urlParams.get('bgVolume')!!) <= 1;
 </script>
 
 <div id="outer">
 	<!-- svelte-ignore a11y-media-has-caption -->
-	{#if showVideo}
+	{#if loadingFiles}
+		<div id="loadingFiles">Loading files...</div>
+	{:else if showVideo}
 		<video
 			in:fade
 			out:fade
@@ -353,5 +428,13 @@
 		width: 140%;
 		overflow: hidden;
 		transform: translate(-15%);
+	}
+	#loadingFiles {
+		height: 100%;
+		width: 100%;
+		font-size: 10vmin;
+		display: flex;
+		align-items: center;
+		justify-content: center;
 	}
 </style>
